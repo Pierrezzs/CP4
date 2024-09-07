ef37f85b3e02e57ff46feaaae0b658e5262ff45c
Gustavo Pierri <pierregustavo39@gmail.com>
2024-09-07 14:35:27 -0300

    Update Nav.jsx

--- vite-project/src/components/Nav.jsx
+++ vite-project/src/components/Nav.jsx
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom';
+import './Nav.css'; // Importa o CSS
 
 function Nav() {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/produtos">Produtos</Link></li>
-        <li><Link to="/sobre">Sobre</Link></li>
-        <li><Link to="/contato">Contato</Link></li>
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li className="nav-item"><Link to="/">Home</Link></li>
+        <li className="nav-item"><Link to="/produtos">Produtos</Link></li>
+        <li className="nav-item"><Link to="/sobre">Sobre</Link></li>
+        <li className="nav-item"><Link to="/contato">Contato</Link></li>
       </ul>
     </nav>
   );
